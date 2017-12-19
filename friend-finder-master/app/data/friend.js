bbd5b60ed603506e1ab91606ca9dc6870db662de
ballerumair <umairasif785@gmail.com>
2017-12-19 03:37:58 -0600

    Add files via upload

---
+++ friend-finder-master/app/data/friend.js
@@ -0,0 +1,20 @@
+
+  {
+    name: 'Umair Asif',
+    profilePic: https://github.com/settings/profile
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
